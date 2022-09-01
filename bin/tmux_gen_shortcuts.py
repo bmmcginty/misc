01e1ff45a1d6329344308d36790eb2de35cfe1ad
Brandon McGinty <git@bmcginty.us>
2022-09-01 08:58:52 -0400

    tmux: support running commands for not-found shortcuts
    If the "Any" shortcut key is specified,
    and a user types any part of a shortcut that does not exist,
    the tmux command specified as the value will be executed.
    This will not happen, however, if the first character of a shortcut is typed incorrectly.
    This avoids altering the root table more than absolutely necissary.
    
    Example:
    f3 prz
    will execute the Any action when the user types z
    f2 prod
    will abort silently
    f3 prod
    will send the shortcut for prod

--- bin/tmux_gen_shortcuts.py
+++ bin/tmux_gen_shortcuts.py
@@ -4,6 +4,10 @@
 Feed the output of this script into .tmux.conf,
 and have shortcuts that will drop text into your current window.
 See ../.tmux.shortcuts.yml for examples.
+Note the case-insensative "any" key. Unlike the other possible shortcut entries,
+the Any shortcut will run the full command specified via value.
+This allows you, to e.g. display a message or ring the terminal's bell via echo,
+when an unsupported key is pressed.
 """
 
 import os
@@ -13,8 +17,12 @@ out = []
 db = yaml.safe_load(open(os.path.expanduser(
     "~/.tmux.shortcuts.yml"), "r").read())
 
+any_already_set = set()
 for entry in db:
     table_name = entry["name"]
+    any_key = [i for i in entry["shortcuts"] if i.lower() == "any"]
+    any_key = any_key[0] if any_key else "any"
+    any = entry["shortcuts"].pop(any_key, None)
     shortcut_names = list(entry["shortcuts"].keys())
     shortcut_names.sort(key=(lambda x: len(x)), reverse=True)
     for _shortcut in shortcut_names:
@@ -35,15 +43,22 @@ for entry in db:
                 frmtbl = "root"
             else:
                 frmtbl = "tbl-%s" % ("-".join(seen),)
+                if any and frmtbl != "root" and frmtbl not in any_already_set:
+                    any_already_set.add(frmtbl)
+                    out.append(("anykey", frmtbl, "Any", any,))
             if len(remainder) == 1:
                 out.append(("key", frmtbl, remainder[0], value,))
                 break
             else:
                 out.append(("tbl", frmtbl, totbl, remainder[0],))
+
 for i in out:
     if i[0] == "tbl":
-        frmtbl, totbl, chr = i[1:]
-        print("tmux bind-key -T %(frmtbl)s %(chr)s switch-client -T%(totbl)s" % vars())
+        frmtbl, totbl, key = i[1:]
+        print("tmux bind-key -T %(frmtbl)s %(key)s switch-client -T%(totbl)s" % vars())
+    elif i[0] == "anykey":
+        frmtbl, key, val = i[1:]
+        print("tmux bind-key -T%(frmtbl)s %(key)s %(val)s" % vars())
     else:
-        frmtbl, chr, val = i[1:]
-        print("tmux bind-key -T%(frmtbl)s %(chr)s send-keys \"%(val)s\"" % vars())
+        frmtbl, key, val = i[1:]
+        print("tmux bind-key -T%(frmtbl)s %(key)s send-keys \"%(val)s\"" % vars())
